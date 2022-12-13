19e84d56194b720fde6a5826928738beab8b67c8
premj44 <premkumarjadhav9044@gmail.com>
2022-12-13 14:52:44 +0530

    a

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const connect = async () => {
-  return mongoose.connect("mongodb+srv://prem:prem@cluster0.keahpj3.mongodb.net/project");
+  return mongoose.connect(process.env.MONGO_URL)
 };
 module.exports = connect;
